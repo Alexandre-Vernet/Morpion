629d80422d2d4f801c77c5d5c60ea5a1f0b9adfd
Alexandre-Vernet <alexandre.vernet@g-mail.fr>
2020-10-23 18:46:29 +0200

    Touche entrée pour actionner le bouton du nom du joueur

--- app/src/main/java/com/ynov/vernet/morpion/nomJoueurActivity.java
+++ app/src/main/java/com/ynov/vernet/morpion/nomJoueurActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -39,12 +40,33 @@ public class nomJoueurActivity extends AppCompatActivity {
                 SharedPreferences prefs = getPreferences(Context.MODE_PRIVATE);
                 SharedPreferences.Editor editor = prefs.edit();
                 editor.putString("nom", "" + nomJoueur1.getText());
-                editor.commit();
+                editor.apply();
 
                 // Démarrer l'activité principale
                 Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                 startActivity(intent);
                 finish();
+            }
+        });
+
+        // Si le joueur appui sur le bouton entrée de son clavier
+        nomJoueur1.setOnKeyListener(new View.OnKeyListener() {
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                // If the event is a key-down event on the "enter" button
+                if ((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                    // Stocker le nom du joueur saisi précédemment
+                    SharedPreferences prefs = getPreferences(Context.MODE_PRIVATE);
+                    SharedPreferences.Editor editor = prefs.edit();
+                    editor.putString("nom", "" + nomJoueur1.getText());
+                    editor.apply();
+
+                    // Démarrer l'activité principale
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                    return true;
+                }
+                return false;
             }
         });
     }
